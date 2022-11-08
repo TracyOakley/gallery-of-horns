43b13ded152bf8e9cbf5710e15a23fdc5ee83872
TracyOakley <tloak12@gmail.com>
2022-11-08 00:39:03 +0000

    AddingLab1

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Footer extends React.Component{
+  render() {
+    return (
+       <p>Tracy Oakley created this React App</p>
+    );
+  }
+}
+
+export default Footer;
---
+++ src/HornedBeast.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class HornedBeast extends React.Component{
+  render(){
+    return(
+      <>
+      <h2>{this.props.title}</h2>
+      <img 
+      height = "300px" 
+      width = "300 px" 
+      alt={this.props.description} 
+      src={this.props.imageURL}
+      title = {this.props.title} />
+      <p>{this.props.description} </p>
+      </>
+    );
+  }
+}
+
+export default HornedBeast;
